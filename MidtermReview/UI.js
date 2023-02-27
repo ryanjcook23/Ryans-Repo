8878b32758e08fffac8b188db179b7a9ecfcb9d9
ryanjcook23 <122556023+ryanjcook23@users.noreply.github.com>
2023-02-27 16:30:08 -0500

    midterm algorrithm

---
+++ MidtermReview/UI.js
@@ -0,0 +1,19 @@
+//create 6 divs with diff words,maybe using array?
+function change(){
+    let x = document.getElementById("div")
+    var xHeight = "100px"
+    var xWidth = "100px"
+x.style.width = xWidth
+x.style.height = xHeight
+x.style.backgroundColor = "red"
+x.style.fontSize = "28px"
+
+}
+
+
+
+
+//make onclick event for all of them to change color and bold txt
+
+//once another is clicked revert back initial
+//make another div to be linked to the onclick event
