cab6e0eeeca4c966cb2022dd83db5f8a64e04944
ryanjcook23 <122556023+ryanjcook23@users.noreply.github.com>
2023-03-02 11:30:06 -0500

    midtemr exam

---
+++ MidtermExam/assignment 1.js
@@ -0,0 +1,7 @@
+let myarray = ["Cirro", "Cumulo","Nimbo", " strato"]
+
+
+for(let i = 0; i<4;i++){
+
+document.getElementById("Loopy").innerHTML += myarray[i] + " "+ "and" + "<br />"
+}
---
+++ MidtermExam/assignment2.js
@@ -0,0 +1,16 @@
+function Reveal(){
+
+  if(onclick){
+
+   
+
+for(let i = 0; i=5; i++ ){
+
+  document.getElementById("reveal").innerHTML = "revealed"
+
+}
+  }
+    
+}
+///using a loop to tell how many clicks have been made??
+//another if statment to find when i is on 5th click to change text??
