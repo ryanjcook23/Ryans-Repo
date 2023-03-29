8790f5f95cdc9ce4d26654f53d286d610ee57b8d
ryanjcook23 <122556023+ryanjcook23@users.noreply.github.com>
2023-03-29 18:06:20 -0400

    algorithm and working on assignments

---
+++ functions/DivisibleBy7.js
@@ -0,0 +1,8 @@
+function verify(){
+    if(calculate.value % 7 == 0){
+document.getElementById("output").innerHTML = "true"
+    }
+    else{
+        document.getElementById("output").innerHTML = "false"
+    }
+}
---
+++ functions/MakeRandom.js
@@ -0,0 +1,8 @@
+function random(){
+   let math = Math.floor(Math.random()* ((11))) 
+   
+   document.getElementById("output").innerHTML = math
+
+console.log(math)
+}
+
