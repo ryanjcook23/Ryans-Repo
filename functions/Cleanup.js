674c0bb9133d548f711c19272023031f821b692e
ryanjcook23 <122556023+ryanjcook23@users.noreply.github.com>
2023-03-30 22:25:24 -0400

    finishing touches on assignment

--- functions/Cleanup.js
+++ functions/Cleanup.js
@@ -1,3 +1,6 @@
 function change(){
-    
+   let value = input.value.replaceAll("#","")
+   document.getElementById("return").innerHTML = value
+
+   console.log(value)
 }
--- functions/DivisibleBy7.js
+++ functions/DivisibleBy7.js
@@ -1,9 +1,13 @@
+
+
 function verify(){
     if(calculate.value % 7 == 0){
-document.getElementById("output").innerHTML = "true"
-    }
-    else{
-        document.getElementById("output").innerHTML = "false"
-    }
+        document.getElementById("output").innerHTML = "true"
+            }
+            else{
+                document.getElementById("output").innerHTML = "false"
+            }
 }
+
+
 
